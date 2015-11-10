3c1ad0864ebd9355dcb5716858dd85428045ec6f
GT <GT>
2015-11-10 19:21:12 -0300

    agrego spring-security

--- Ejercicio1/grails-app/conf/BootStrap.groovy
+++ Ejercicio1/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,22 @@
+import com.qb9.examen.Role
+import com.qb9.examen.User
+import com.qb9.examen.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
-    }
+    def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+    def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+ 
+    def testUser = new User(username: 'me', password: 'password')
+    testUser.save(flush: true)
+ 
+    UserRole.create testUser, adminRole, true
+ 
+    assert User.count() == 1
+    assert Role.count() == 2
+    assert UserRole.count() == 1
+}
     def destroy = {
     }
 }
