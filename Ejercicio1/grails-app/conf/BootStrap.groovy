918db06de1134dbc3798c0dbc062431158214364
GT <GT>
2015-11-10 23:18:39 -0300

    agrego valores dummys en startup

--- Ejercicio1/grails-app/conf/BootStrap.groovy
+++ Ejercicio1/grails-app/conf/BootStrap.groovy
@@ -1,3 +1,6 @@
+import java.awt.event.ItemEvent;
+
+import com.qb9.examen.Item
 import com.qb9.examen.Role
 import com.qb9.examen.User
 import com.qb9.examen.UserRole
@@ -23,6 +26,23 @@ class BootStrap {
     assert User.count() == 2
     assert Role.count() == 2
     assert UserRole.count() == 2
+	
+	// agrego valores dummys en startup
+	def item = new Item();
+	item.setDesc("VD1")
+	item.setValor(1)
+	item.save();
+	
+	def item1 = new Item();
+	item1.setDesc("VD2")
+	item1.setValor(2)
+	item1.save();
+	
+	def item2 = new Item();
+	item2.setDesc("VD3")
+	item2.setValor(3)
+	item2.save();
+	
 }
     def destroy = {
     }
